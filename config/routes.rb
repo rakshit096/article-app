6e0175245bd3fc517ce8c0fe6b18f32adeb3e418
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-10 13:26:40 +0530

    chore: add resources instructions and usage comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
- resources :articles
+ resources :articles                              
 end
+
+
+
+
+
+
+# Note
+# Only generate index, show, new, and create routes
+# resources :articles, only: [:index, :show, :new, :create]
+
+# Generate all routes except destroy
+# resources :articles, except: [:destroy]
