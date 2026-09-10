63b8511e5abaf2124be2d692bd4eec72e264bb88
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-10 12:42:46 +0530

    feat: get single article by id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all  # this line will get all articles from database. Also @articles is instance variable so rails make it availabe for view.
   end
+
+  def show
+  @article = Article.find(params[:id])
+  end
+
 end
